ea68d329e894d56e776d7b6ca3454e5339f2c283
MohamadSafi <the.mohammad.safi@gmail.com>
2023-05-10 16:28:06 +0300

    adding useNetwork to walletbar

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -1,7 +1,37 @@
+import { useEffect } from "react";
+import useSWR from "swr";
+
+const NETWORKS = {
+  1: "Ethereum Main Network",
+  3: "Ropsten Test Network",
+  4: "Rinkeby Test Network",
+  5: "Goerli Test Network",
+  42: "Kovan Test Network",
+  56: "Binance Smart Chain",
+  1337: "Ganache",
+  11155111: "Sepolia test network",
+};
+
 export const handler = (web3, provider) => () => {
+  const { mutate, ...rest } = useSWR(
+    () => (web3 ? "web3/network" : null),
+    async () => {
+      const chainId = await web3.eth.getChainId();
+      return NETWORKS[chainId];
+    }
+  );
+
+  useEffect(() => {
+    provider &&
+      provider.on("chainChanged", (chainId) => {
+        mutate(NETWORKS[parseInt(chainId, 16)]);
+      });
+  }, [web3]);
+
   return {
     network: {
-      data: "Testing Network",
+      mutate,
+      ...rest,
     },
   };
 };
---
+++ components/ui/course/card/index.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Card({ course }) {
+  return (
+    <div className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <div className="flex h-full w-auto">
+        <Image
+          className="object-cover"
+          src={course.coverImage}
+          width="200"
+          height="230"
+          alt={course.title}
+        />
+        <div className="p-8">
+          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+            {course.type}
+          </div>
+          <Link legacyBehavior href={`/courses/${course.slug}`}>
+            <a className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+              {course.title}
+            </a>
+          </Link>
+          <p className="mt-2 text-gray-500">{course.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
