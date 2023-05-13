2eda41ec6c62e883132010e4f0340a00a5148cb5
MohamadSafi <the.mohammad.safi@gmail.com>
2023-05-13 12:02:52 +0300

    Added Breadcrumps

--- components/ui/common/breadcrumbs/index.js
+++ components/ui/common/breadcrumbs/index.js
@@ -1,15 +1,15 @@
-export default function breadcrumbs() {
+export default function Breadcrumbs() {
   return (
-    <nav aria-label="breadcrumb" className="mb-4">
-      <ol className="flex leading-none text-white divide-x divide-white">
-        <li className="pr-4">
+    <nav aria-label="breadcrumb">
+      <ol className="flex leading-none text-black divide-x divide-black">
+        <li className="pr-4 font-medium text-gray-500 hover:text-gray-900">
           <a href="#">Buy</a>
         </li>
-        <li className="px-4">
-          <a href="#">My Orders</a>
+        <li className="px-4 font-medium text-gray-500 hover:text-gray-900">
+          <a href="#">My Courses</a>
         </li>
-        <li className="px-4">
-          <a href="#">All Orders</a>
+        <li className="px-4 font-medium text-gray-500 hover:text-gray-900">
+          <a href="#">Manage Courses</a>
         </li>
       </ol>
     </nav>
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -3,10 +3,9 @@ import { BaseLayout } from "@/components/ui/common/layout";
 import { getAllCourses } from "@/content/courses/fetcher";
 import WalletBar from "@/components/ui/web3/walletbar";
 import EthRates from "@/components/ui/web3/ethRates";
-import { Button } from "@/components/ui/common";
+import { Breadcrumbs, Button } from "@/components/ui/common";
 import { OrderModal } from "@/components/ui/order";
 import { useState } from "react";
-import { useAccount, useNetwork } from "@/components/web3/hooks";
 
 export default function Marketplace({ courses }) {
   const [selectedCourse, setSelectedCourse] = useState(null);
@@ -14,6 +13,9 @@ export default function Marketplace({ courses }) {
     <BaseLayout>
       <WalletBar />
       <EthRates />
+      <div className="py-4 px-4 sm:px-6 lg:px-8 flex justify-center">
+        <Breadcrumbs />
+      </div>
       <List courses={courses}>
         {(course) => (
           <CourseCard
