7952ed565626863a1af68a1d0f19ed6ba5ba846b
MohamadSafi <the.mohammad.safi@gmail.com>
2023-05-13 20:08:05 +0300

    form status

--- components/ui/order/modal/index.js
+++ components/ui/order/modal/index.js
@@ -8,6 +8,23 @@ const defaultOrder = {
   confirmationEmail: "",
 };
 
+const _createFormState = (isDisabled = false, message = "") => ({
+  isDisabled,
+  message,
+});
+
+const createFormState = ({ price, email, confirmationEmail }) => {
+  if (!price || Number(price) <= 0) {
+    return _createFormState(true, "Price is not valid.");
+  } else if (confirmationEmail.length === 0 || email.length === 0) {
+    return _createFormState(true);
+  } else if (email !== confirmationEmail) {
+    return _createFormState(true, "Email are not matching.");
+  }
+
+  return _createFormState();
+};
+
 export default function OrderModal({ course, onClose }) {
   const [isOpen, setIsOpen] = useState(false);
   const [order, setOrder] = useState(defaultOrder);
@@ -29,6 +46,8 @@ export default function OrderModal({ course, onClose }) {
     setOrder(defaultOrder);
     onClose();
   };
+
+  const formState = createFormState(order);
 
   return (
     <Modal isOpen={isOpen}>
@@ -139,11 +158,17 @@ export default function OrderModal({ course, onClose }) {
                   not correct
                 </span>
               </div>
+              {formState.message && (
+                <div className="p-4 my-3 text-red-700 bg-red-200 rounded-lg text-sm">
+                  {formState.message}
+                </div>
+              )}
             </div>
           </div>
         </div>
         <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex">
           <Button
+            disabled={formState.isDisabled}
             onClick={() => {
               alert(JSON.stringify(order));
             }}
