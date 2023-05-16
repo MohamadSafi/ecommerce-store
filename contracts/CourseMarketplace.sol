4fee86c2d868747c0e03ccfb3c23b2ecd73c12d4
MohamadSafi <the.mohammad.safi@gmail.com>
2023-05-16 17:41:05 +0300

    Marketplace contract

---
+++ contracts/CourseMarketplace.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract CourseMarketplace {
+
+  enum State {
+    Purchased,
+    Activated,
+    Deactivated
+  }
+
+  struct Course {
+    uint id; // 32
+    uint price; // 32
+    bytes32 proof; // 32
+    address owner; // 20
+    State state; // 1
+  }
+
+  // mapping of courseHash to Course data
+  mapping(bytes32 => Course) private ownedCourses;
+
+  // mapping of courseID to courseHash
+  mapping(uint => bytes32) private ownedCourseHash;
+
+  // number of all courses + id of the course
+  uint private totalOwnedCourses;
+
+  function purchaseCourse(
+    bytes16 courseId, // 0x00000000000000000000000000003130
+    bytes32 proof // 0x0000000000000000000000000000313000000000000000000000000000003130
+  )
+    external
+    payable
+  {
+    bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
+    uint id = totalOwnedCourses++;
+
+    ownedCourseHash[id] = courseHash;
+    ownedCourses[courseHash] = Course({
+      id: id,
+      price: msg.value,
+      proof: proof,
+      owner: msg.sender,
+      state: State.Purchased
+    });
+  }
+}
